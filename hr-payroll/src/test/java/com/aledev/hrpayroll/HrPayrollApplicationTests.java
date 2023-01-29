a5d1459ff6f194e2c31499bb5837c22a9c5c578d
Alessandro <Alessandro.souza@edu.pucrs.br>
2023-01-29 03:09:17 -0300

    Initial Commit

---
+++ hr-payroll/src/test/java/com/aledev/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aledev.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
