09fa4b3a8ef2d2d4bb613517e7f8eded30860f06
Alessandro <Alessandro.souza@edu.pucrs.br>
2023-01-30 11:19:25 -0300

    Ribbon load balance

--- hr-worker/src/main/java/com/aledev/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/aledev/hrworker/resources/WorkerResource.java
@@ -3,6 +3,9 @@ package com.aledev.hrworker.resources;
 import com.aledev.hrworker.Repositories.WorkerRepository;
 import com.aledev.hrworker.entity.Worker;
 import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,8 +14,11 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/workers")
 @RequiredArgsConstructor
+@Slf4j
 public class WorkerResource {
     private final WorkerRepository workerRepository;
+
+    private final Environment environment;
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
@@ -22,6 +28,8 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public Worker findById(@PathVariable Long id) {
+        log.info("Port = " + environment.getProperty("local.server.port"));
+
         var worker  = workerRepository.findById(id);
         return worker.orElse(Worker.builder().build());
     }
